150b3e6b5978e708232a6b648cbfc7b8d99ad976
Rana Das <rana.pratap.das@gmail.com>
2016-09-24 17:12:32 +0100

    cleanup

--- src/main/java/pl/dmichalski/config/ViewConfig.java
+++ src/main/java/pl/dmichalski/config/ViewConfig.java
@@ -13,28 +13,10 @@ import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 public class ViewConfig {
     @Bean
     public ViewResolver viewResolver() {
-//    public UrlBasedViewResolver viewResolver() {
-//        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
-//        viewResolver.setViewClass(TilesView.class);
-//        return viewResolver;
-
         InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
         viewResolve.setPrefix("/WEB-INF/jsp/");
         viewResolve.setSuffix(".jsp");
 
         return viewResolve;
     }
-
-//    @Bean
-//    public TilesConfigurer tilesConfigurer() {
-//        TilesConfigurer tilesConfigurer = new TilesConfigurer();
-//        tilesConfigurer.setDefinitions(new String[]{
-////                "/WEB-INF/layouts/tiles.xml",
-////                "/WEB-INF/views/**/tiles.xml"
-//                "/WEB-INF/tiles/tiles-definitions.xml"
-//
-//        });
-//        tilesConfigurer.setCheckRefresh(true);
-//        return tilesConfigurer;
-//    }
 }
